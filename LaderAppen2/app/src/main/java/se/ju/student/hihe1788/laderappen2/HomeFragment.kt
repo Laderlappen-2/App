ce5ce15b97aed234c5f0a45776848cfd6e5e48d3
JonnaLennartsson <43805911+JonnaLennartsson@users.noreply.github.com>
2020-04-03 12:04:36 +0200

    started implementing bottomNav

---
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/HomeFragment.kt
@@ -0,0 +1,25 @@
+package se.ju.student.hihe1788.laderappen2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeFragment: Fragment() {
+    private var theView: View? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        if (theView == null) {
+            val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+            theView = view
+        }
+
+        return theView
+    }
+}
--- LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/MainActivity.kt
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/MainActivity.kt
@@ -1,6 +1,37 @@
 package se.ju.student.hihe1788.laderappen2
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
 
+    private lateinit var bottomNavigation: BottomNavigationView
+    private val onNavItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.naviation_settings -> {
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.navigation_home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.navigation_routes -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavItemSelectedListener)
+
+
+    }
 }
