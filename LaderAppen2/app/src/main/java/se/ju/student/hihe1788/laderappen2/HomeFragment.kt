d2710876e519adbb1d27cc7170a5290cddd4321f
JonnaLennartsson <43805911+JonnaLennartsson@users.noreply.github.com>
2020-04-08 08:05:18 +0200

    ready to be merged with view

--- LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/HomeFragment.kt
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.*
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment: Fragment() {
     override fun onCreateView(
@@ -20,11 +22,14 @@ class HomeFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.bntDrive)?.setOnClickListener {
+
+            //TODO needs to wait for acceptance and then navigate
             if (!BluetoothHandler.isBluetoothEnabled()) {
                 //check if user wants to enable bluetooth
                 context?.let { it1 -> AlertDialog.acceptBtDialog(it1, "Bluetooth", "You need bluetooth for this.\nDo you want to activate bluetooth?") }
+            } else {
+                findNavController().navigate(R.id.driveFragment, null)
             }
-            //navigate to DriveFragment
         }
     }
 
