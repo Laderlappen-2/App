227bfd0b6c6c0327bef1ac807f8429ce600d2843
Oscar Bjurelid <oscarbjurelid@gmail.com>
2020-04-15 09:28:26 +0200

    LAD-34 LAD-33 #comment Initial commit for restucturing bluetoothService and Handler

--- LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/HomeFragment.kt
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/HomeFragment.kt
@@ -22,14 +22,7 @@ class HomeFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.bntDrive)?.setOnClickListener {
-            /*
-            //TODO needs to wait for acceptance and then navigate
-            if (!BluetoothHandler.isBluetoothEnabled()) {
-                //check if user wants to enable bluetooth
-                context?.let { it1 -> AlertDialog.acceptBtDialog(it1, "Bluetooth", "You need bluetooth for this.\nDo you want to activate bluetooth?") }
-            } else {
-                findNavController().navigate(R.id.driveFragment, null)
-            }*/
+
             findNavController().navigate(R.id.driveFragment, null)
         }
     }
--- LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/MainActivity.kt
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/MainActivity.kt
@@ -28,32 +28,6 @@ import kotlin.collections.ArrayList
 
 public class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-    private var discoveredBtDevices: ArrayList<BluetoothDevice> = ArrayList()
-    private lateinit var ourMower: BluetoothDevice
-    private val MY_UUID = UUID.fromString("c51c605f-63dc-4015-8de5-45525ca7eba0")
-
-    private val receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val action = intent?.action
-            when(action) {
-                BluetoothDevice.ACTION_FOUND -> {
-
-                    val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    val deviceName= device.name
-                    val deviceHardwareAdress = device.address
-                    discoveredBtDevices.add(device)
-
-                    if (device.address == "OUR  MAC") {
-                        ourMower = device
-                        //connect with device
-                        connectWithMower()
-                    }
-
-                }
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,14 +46,11 @@ public class MainActivity : AppCompatActivity() {
         setupActionBar(navController, appBarConfiguration)
         setupBottomNavMenu(navController)
 
-        /*---------------------[  REGISTER RECEIVERS  ]-----------------------------*/
-        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(receiver)
     }
 
     /*---------------------[  SET UP VIEWS AND NAVIGATION  ]-----------------------------*/
@@ -100,74 +71,6 @@ public class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return findNavController(R.id.my_nav_host_fragment).navigateUp(appBarConfiguration)
-    }
-
-    /*---------------------[  BLUETOOTH STUFF  ]-----------------------------*/
-
-    fun isBluetoothEnabled(): Boolean {
-        if (bluetoothAdapter?.isEnabled == false) {
-            return false
-        }
-        return true
-    }
-
-    fun enableBluetooth() {
-        if (bluetoothAdapter == null) {
-            // device does not support Bluetooth -> send msg to user
-            //TODO give user alertDialog
-        }
-
-        if (bluetoothAdapter?.isEnabled == false) {
-            bluetoothAdapter.enable()
-        } else {
-            bluetoothAdapter?.disable()
-        }
-    }
-
-    fun startBtConnect() {
-        // if not bonded with mower
-        if (!isPairedWithMower()) {
-            bluetoothAdapter?.startDiscovery()
-        } else {
-            /*bonded*/
-            connectWithMower()
-        }
-    }
-
-    private fun isPairedWithMower(): Boolean {
-        val pairedDevices: MutableSet<BluetoothDevice> = bluetoothAdapter?.bondedDevices ?: return false
-        return pairedDevices.contains(ourMower)
-    }
-
-    private fun connectWithMower() {
-        ConnectThread(ourMower).start()
-    }
-
-    private inner class ConnectThread(device: BluetoothDevice) : Thread() {
-
-        private val socket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
-            device.createRfcommSocketToServiceRecord(MY_UUID)
-        }
-
-        public override fun run() {
-            bluetoothAdapter?.cancelDiscovery()
-
-            socket?.use { socket ->
-                socket.connect()
-
-                // Perhaps initBluetoothService(handler, socket)
-                // start initConnectedThread(socket)
-            }
-        }
-
-        fun cancel() {
-            try {
-                socket?.close()
-            } catch (e: IOException) {
-                print("ConnectThread could not close. Msg: $e")
-            }
-        }
-
     }
 
 }
