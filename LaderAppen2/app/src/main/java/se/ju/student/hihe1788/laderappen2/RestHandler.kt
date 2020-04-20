e142cc37bd1d2a7e0f797a03c5e342c035d02eb2
JonnaLennartsson <43805911+JonnaLennartsson@users.noreply.github.com>
2020-04-20 15:48:23 +0200

    LAD-112 #comment 'started implementing RestHandler' #time 2h 30m

---
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/RestHandler.kt
@@ -0,0 +1,38 @@
+package se.ju.student.hihe1788.laderappen2
+
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.beust.klaxon.Klaxon
+
+object RestHandler {
+    val REST_URL = "https://laderlappen-2-rest-api.herokuapp.com/v1/"
+    val DRIVE_SESSION = "drivingsessions"
+    val ALL_DRIVE_SESSIONS = "drivingsessions?from=1&limit=10"
+    val DRIVE_SESSION_BY_ID = "drivingsessions/:sessionId"
+    val EVENT = "events"
+
+    fun getAllRoutes() {
+        // get all routes
+        val url = REST_URL+ ALL_DRIVE_SESSIONS
+        
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            // on success
+            Response.Listener {
+                //do stuff with response
+            },
+
+            Response.ErrorListener {
+
+            })
+
+        RequestQueueSingleton.getInstance(MainActivity.appContext).addToRequestQueue(jsonObjectRequest)
+    }
+
+    fun postRoute() {
+        // first post session
+        // receive id for that session
+        // loop through points in Route
+            // -> for each point: post event
+    }
+}
