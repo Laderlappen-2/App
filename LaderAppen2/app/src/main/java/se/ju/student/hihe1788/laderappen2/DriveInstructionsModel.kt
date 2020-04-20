54b19b9fc9551b2ed072ff2be36596e9244d36af
Henric Hiljanen <hihe1788@student.ju.se>
2020-04-20 17:34:45 +0200

    LAD-100 extend DriveInstructionsModel

--- LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/DriveInstructionsModel.kt
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/DriveInstructionsModel.kt
@@ -1,12 +1,35 @@
 package se.ju.student.hihe1788.laderappen2
 
 class DriveInstructionsModel {
+    private val MIN_THRUST = -100
+    private val MAX_THRUST = 100
+    private val MIN_TURN = -100
+    private val MAX_TURN = 100
+
     var mThrust: Int = 0
     var mTurn: Int = 0
 
     fun setInstructions(thrust: Int, turn: Int) {
-        mThrust = thrust
-        mTurn = turn
+        setThrust(thrust)
+        setTurn(turn)
+    }
+
+    fun setThrust(thrust: Int) {
+        if (thrust < MIN_THRUST)
+            mThrust = MIN_THRUST
+        else if (thrust > MAX_THRUST)
+            mThrust = MAX_THRUST
+        else
+            mThrust = thrust
+    }
+
+    fun setTurn(turn: Int) {
+        if (turn < MIN_TURN)
+            mThrust = MIN_TURN
+        else if (turn > MAX_TURN)
+            mThrust = MAX_TURN
+        else
+            mThrust = turn
     }
 
 
