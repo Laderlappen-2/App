cd7e12a8ab4998d9ee5f4d5759eff0365bbc6934
Henric Hiljanen <hihe1788@student.ju.se>
2020-05-02 13:24:28 +0200

    LAD-123 #comment Changed DriveInstructionsModel to include light and honk

--- LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/DriveInstructionsModel.kt
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/DriveInstructionsModel.kt
@@ -4,47 +4,71 @@ package se.ju.student.hihe1788.laderappen2
  * Represent the mowers current state
  */
 object DriveInstructionsModel {
-    private val MIN_THRUST = -100
-    private val MAX_THRUST = 100
-    private val MIN_TURN = -100
-    private val MAX_TURN = 100
+    private val MIN_THRUST = 0f
+    private val MAX_THRUST = 200f
+    private val NEUTRAL_THRUST= (MAX_THRUST - MIN_THRUST) / 2f
+    private val MIN_TURN = 0f
+    private val MAX_TURN = 200f
+    private val NEUTRAL_TURN = (MAX_TURN - MIN_TURN) / 2f
 
     var mThrust: Int = 0
     var mTurn: Int = 0
-
-    /**
-     * @param value A given value from -1:1
-     * @param isThrust Sets if this new instruction is thrust or steer.
-     */
-    fun setInstructions(value: Float, isThrust: Boolean) {
-        val transValue: Int = translateToRatio(value)
-        if (isThrust) setThrust(transValue)
-        else setTurn(transValue)
-
-    }
+    var mLight: Int = 0
+    var mHonk: Int = 0
+    var mAuto: Int = 0
 
     /**
      * @param thrust sets a new thrust
      */
-    private fun setThrust(thrust: Int) {
-        if (thrust < MIN_THRUST)
-            mThrust = MIN_THRUST
-        else if (thrust > MAX_THRUST)
-            mThrust = MAX_THRUST
+    private fun setThrust(percent: Float) {
+        val value = NEUTRAL_THRUST * percent
+        val newThrust = (NEUTRAL_THRUST + value)
+
+        if (newThrust < MIN_THRUST)
+            mThrust = MIN_THRUST.toInt()
+        else if (newThrust > MAX_THRUST)
+            mThrust = MAX_THRUST.toInt()
         else
-            mThrust = thrust
+            mThrust = newThrust.toInt()
     }
 
     /**
      * @param turn sets a new turn
      */
-    private fun setTurn(turn: Int) {
-        if (turn < MIN_TURN)
-            mThrust = MIN_TURN
-        else if (turn > MAX_TURN)
-            mThrust = MAX_TURN
+    private fun setTurn(percent: Float) {
+        val value = NEUTRAL_TURN * percent
+        val newTurn = (NEUTRAL_TURN + value)
+
+        if (newTurn < MIN_TURN)
+            mTurn = MIN_TURN.toInt()
+        else if (newTurn > MAX_TURN)
+            mTurn = MAX_TURN.toInt()
         else
-            mThrust = turn
+            mTurn = newTurn.toInt()
+    }
+
+    fun setLightOn() {
+        mLight = 1
+    }
+
+    fun setLightOff() {
+        mLight = 0
+    }
+
+    fun setHonkOn() {
+        mHonk = 1
+    }
+
+    fun setHonkOff() {
+        mHonk = 0
+    }
+
+    fun setAutoOn() {
+        mAuto = 1
+    }
+
+    fun setAutoOff() {
+        mAuto = 0
     }
 
     /**
@@ -59,16 +83,9 @@ object DriveInstructionsModel {
      * $: end of message
      * @return ByteArray consisting of the new instruction(s)
      */
-    fun toMessage(): ByteArray {
-        return "@$mTurn,$mThrust$".toByteArray()
-    }
-
-    /**
-     * Sets the joysticks value to a value that the mower understands.
-     * @param value The value from the joystick
-     * @return A value that the mower understands
-     */
-    private fun translateToRatio(value: Float) : Int {
-        return (value * 100).toInt()
+    fun toByteArray(): ByteArray {
+        val bArr = "@$mTurn;$mThrust;$mLight;$mHonk$".toByteArray()
+        mHonk = 0
+        return bArr
     }
 }
