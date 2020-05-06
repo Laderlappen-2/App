eee9c94b4a4ca6b3ff0dd721b63a630eaf5a3ef9
Adam <a.clettborn@gmail.com>
2020-05-06 11:14:23 +0200

    LAD-109 #time 40m #comment Implemented set steering mode in settings

--- LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/util/SharedPreferencesManager.kt
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/util/SharedPreferencesManager.kt
@@ -7,11 +7,28 @@ import androidx.core.content.edit
 class SharedPreferencesManager(context: Context) {
     private val PREFERENCES_NAME = "app_settings"
     private val DEVICE_LIST = "devices"
+    private val STEERING_MODE = "steeringMode"
+    private val DEFAULT_STEERING_MODE = SteeringModeEnum.TWO_HANDED
 
     private var sharedPrefs: SharedPreferences
 
     init {
         sharedPrefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+    }
+
+    fun setSteeringMode(steeringMode: SteeringModeEnum) {
+        sharedPrefs.edit {
+            this.putInt(STEERING_MODE, steeringMode.value)
+            this.commit()
+        }
+    }
+
+    fun getSteeringMode(): SteeringModeEnum {
+        var steeringMode = SteeringModeEnum.fromValue(sharedPrefs.getInt(STEERING_MODE, DEFAULT_STEERING_MODE.value))
+        steeringMode?.let {
+            return it
+        }
+        return DEFAULT_STEERING_MODE
     }
 
     fun getDevices(): MutableSet<String> {
