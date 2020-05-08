bd04fd1b6583f5342be9c856de2838d1b64681eb
JonnaLennartsson <43805911+JonnaLennartsson@users.noreply.github.com>
2020-05-08 09:20:20 +0200

    LAD-41 #comment made some changes to the move func. works ok now. #time 1h 15m

--- LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/RouteCanvasView.kt
+++ LaderAppen2/app/src/main/java/se/ju/student/hihe1788/laderappen2/RouteCanvasView.kt
@@ -6,30 +6,25 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.PointF
 import android.util.AttributeSet
-import android.util.Log
-import android.view.GestureDetector
-import android.view.MotionEvent
-import android.view.ScaleGestureDetector
+import android.view.*
 import android.view.ScaleGestureDetector.OnScaleGestureListener
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.plus
-import androidx.core.view.MotionEventCompat
-import androidx.core.view.MotionEventCompat.getActionIndex
-import androidx.core.view.MotionEventCompat.getActionMasked
-
-import kotlin.math.log2
-
 
 class RouteCanvasView(context: Context, attrs: AttributeSet): View(context, attrs) {
     private var mScaleDetector: ScaleGestureDetector? = null
-    private var mSimpleGestureDetector: GestureDetector? = null
-
     private var mSortedPoints = ArrayList<PointModel>()
     private var mPositionPoints = ArrayList<PointModel>()
     private var mCollisionAvoidancePoints = ArrayList<PointModel>()
     private lateinit var mOrigin: PointF
+    private var mScale: Float = 10f
+    private var mOffset: PointF = PointF(0f, 0f)
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+    private var currentX = 0f
+    private var currentY = 0f
+
     private lateinit var mExtraCanvas: Canvas
     private lateinit var mExtraBitmap: Bitmap
     private val mBackgroundColor = ResourcesCompat.getColor(resources, R.color.colorWhite, null)
@@ -37,6 +32,7 @@ class RouteCanvasView(context: Context, attrs: AttributeSet): View(context, attr
     private val mDrawCollisionColor = ResourcesCompat.getColor(resources, R.color.colorSecondary, null)
     private val mDrawStartColor = ResourcesCompat.getColor(resources, R.color.colorGreen, null)
     private val mDrawStopColor = ResourcesCompat.getColor(resources, R.color.colorRed, null)
+
     private val mLinePaint  = Paint().apply {
         color = mDrawPositionColor
         isAntiAlias = true
@@ -46,13 +42,10 @@ class RouteCanvasView(context: Context, attrs: AttributeSet): View(context, attr
         strokeCap = Paint.Cap.ROUND
         strokeWidth = 8f
     }
-
     private val mPositionPaint = Paint().apply { color = mDrawPositionColor }
     private val mCollisionPaint = Paint().apply { color = mDrawCollisionColor }
     private val mStartPositionPaint = Paint().apply { color = mDrawStartColor }
     private val mStopPositionPaint = Paint().apply { color = mDrawStopColor }
-    private var mScale: Float = 10f
-    private var mOffset: PointF = PointF(0f, 0f)
 
     fun updatePoints(route: RouteModel) {
         mPositionPoints = route.positionEvents
@@ -94,30 +87,39 @@ class RouteCanvasView(context: Context, attrs: AttributeSet): View(context, attr
                 return false
             }
         })
-
-        mSimpleGestureDetector = GestureDetector(context, object :
-            GestureDetector.SimpleOnGestureListener() {
-                override fun onScroll(
-                    e1: MotionEvent?,
-                    e2: MotionEvent?,
-                    distanceX: Float,
-                    distanceY: Float
-                ): Boolean {
-                    Log.d("SCROLL", ""+ distanceX)
-                    mOffset.plus(PointF(distanceX, distanceY))
-                    Log.d("SCROLL", "Offset " + mOffset.x + ":" + mOffset.y)
-                    return super.onScroll(e1, e2, distanceX, distanceY)
-                }
-            }
-        )
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         mScaleDetector!!.onTouchEvent(event)
-        mSimpleGestureDetector!!.onTouchEvent(event)
+
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+        }
 
         invalidate()
         return true
+    }
+
+    private fun touchStart() {
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+
+            mOffset.set((dx + currentX).minus(mOrigin.x), (dy + currentY).minus(mOrigin.y))
+
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+        }
+        invalidate()
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -143,11 +145,9 @@ class RouteCanvasView(context: Context, attrs: AttributeSet): View(context, attr
 
             when (index) {
                 0 -> { canvas?.drawCircle(position.x, position.y, 35f, mStartPositionPaint) }
-                mPositionPoints.size -> { canvas?.drawCircle(position.x, position.y, 35f, mStopPositionPaint) }
+                (mPositionPoints.size -1 ) -> { canvas?.drawCircle(position.x, position.y, 35f, mStopPositionPaint) }
                 else -> { canvas?.drawCircle(position.x, position.y, 20f, mPositionPaint) }
             }
         }
-
-        /*MIGHT NEED THIS CRAP canvas?.(mExtraBitmap, 0f, 0f, null)*/
     }
 }
